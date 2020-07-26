748f69b1e80cfdb5bf4065dcbb5e7684c71f72d5
Deng mou <dengmou@VODDLE.local>
2020-07-26 14:49:40 +0800

    find back

---
+++ stick_frontend/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginView.swift
+//  stick_frontend
+//
+//  Created by Deng mou on 2020/7/26.
+//  Copyright © 2020 Deng mou. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
